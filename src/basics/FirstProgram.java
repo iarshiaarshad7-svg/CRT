db7022b22845b923ffeb2593060be009fd01792d
iarshiaarshad7 <i.arshiaarshad7@gmail.com>
2026-09-02 10:06:38 +0530

    -sum of two numbers

--- src/basics/FirstProgram.java
+++ src/basics/FirstProgram.java
@@ -1,5 +1,11 @@
 package basics;
 
 public class FirstProgram {
+    public static void main (String[] args){
+        int a=10;
+        int b=20;
+        int c=a+b;
+        System.out.println("sum of two numbers:" + c);
 
+    }
 }
