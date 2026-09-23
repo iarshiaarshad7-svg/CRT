3228030ab3856b18dea4da3a9a2bc3477aa7e380
iarshiaarshad7 <i.arshiaarshad7@gmail.com>
2026-09-23 10:20:23 +0530

    switch-case

---
+++ src/basics/problem1.java
@@ -0,0 +1,21 @@
+package basics;
+public class problem1 {
+    public static void main(String[] args) {
+        int dayNumber=2;
+        switch(dayNumber) {
+            case 1:
+                System.out.println("MOnday");
+                break;
+            case 2:
+                System.out.println("tueday");
+                break;
+            case 3:
+                System.out.println("wednesday");
+                break;
+            default:
+                System.out.println("Invalid day");
+        }
+
+        }
+    }
+
